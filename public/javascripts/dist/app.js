211ad848c0d37b6d425cb07c778b4790da7d455e
David Saunders <david@monkee-boy.com>
2017-11-01 10:26:56 -0500

    Updated datepicker maxdate param; Hide day selector of datepicker to limit user to picking month and year

--- public/javascripts/dist/app.js
+++ public/javascripts/dist/app.js
@@ -179,22 +179,4 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
   }
 
   changeBudgetColor();
-
-  // const svg = d3.select("svg.line-chart"),
-  // margin = {top: 20, right: 20, bottom: 30, left: 50},
-  // width = +svg.attr("width") - margin.left - margin.right,
-  // height = +svg.attr("height") - margin.top - margin.bottom,
-  // g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-  // var parseTime = d3.timeParse("%d-%b-%y");
-
-  // var x = d3.scaleTime()
-  //     .rangeRound([0, width]);
-
-  // var y = d3.scaleLinear()
-  //     .rangeRound([height, 0]);
-
-  // var line = d3.line()
-  //     .x(function(d) { return x(d.date); })
-  //     .y(function(d) { return y(d.close); });
 })();
